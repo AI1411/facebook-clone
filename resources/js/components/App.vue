b89b6d92ab08e577206ddaa3e418b79dc1e5f87b
AI1411 <goethe0719@gmail.com>
2020-05-31 23:10:12 +0900

    init

---
+++ resources/js/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Start />
+    </div>
+</template>
+
+<script>
+    import Start from "../views/Start";
+    export default {
+        components: {
+            Start
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
