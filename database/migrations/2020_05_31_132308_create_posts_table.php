0deee8abf6b4e086b87f8bb34763db9fca316f20
AI1411 <goethe0719@gmail.com>
2020-06-01 00:43:49 +0900

    テスト追加

--- database/migrations/2020_05_31_132308_create_posts_table.php
+++ database/migrations/2020_05_31_132308_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->string('body');
+            $table->text('body');
             $table->timestamps();
         });
     }
