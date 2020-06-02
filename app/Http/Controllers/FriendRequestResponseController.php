fb50fd9b8b8c28cc40745b18c9be204c883a3c0a
AI1411 <goethe0719@gmail.com>
2020-06-02 10:09:01 +0900

    友達申請成功のテスト追加

---
+++ app/Http/Controllers/FriendRequestResponseController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\FriendResource;
+use App\Models\Friend;
+use Illuminate\Http\Request;
+
+class FriendRequestResponseController extends Controller
+{
+    public function store()
+    {
+        $data = request()->validate([
+            'user_id' => '',
+            'status' => ''
+        ]);
+
+        $friendRequest = Friend::where('user_id', $data['user_id'])
+            ->where('friend_id', auth()->user()->id)
+            ->firstOrFail();
+
+        $friendRequest->update(array_merge($data, [
+            'confirmed_at' => now()
+        ]));
+
+        return new FriendResource($friendRequest);
+
+    }
+}
